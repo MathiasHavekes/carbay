498dccd00d77ddfa02c9faf2c0277a78636e29dd
Hadjeres Amar <Planteramartin@gmail.com>
2021-02-26 16:41:17 -0500

    rajout du code pour la btable de voiture dans rentCar.js

--- routes/rentCar.js
+++ routes/rentCar.js
@@ -1,6 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const nbVoiture = 4;
+var tableVoitures = [];
 
+
+class Car {
+  constructor(Plaque, modele, couleur, etat) {
+    this.Plaque = Plaque;
+    this.modele = modele;
+    this.couleur = couleur;
+    this.etat = etat;
+  }
+}
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "testdb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM voitures", function (err, result, fields) {
+    if (err) throw err;
+
+    for(i=0 ; i < nbVoiture; i++)
+    {
+      tableVoitures.push(new Car(result[i].Plaque, result[i].modele, result[i].couleur, result[i].etat));
+    }
+    console.log(tableVoitures);
+  });
+
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('rentCar');
